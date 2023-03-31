363688373a90726d52d6f7b4d6b3f49f15b1d326
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-01 06:31:49 +0900

    test

---
+++ TestIfElse.java
@@ -0,0 +1,13 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		int x = 30;
+		if(x < 20) {
+			System.out.println("这是if语句");
+		} else {
+			System.out.println("这是else语句");
+		}
+//		if(x < 20) {
+//			System.out.println("这是if语句");
+//		}
+	}
+}
