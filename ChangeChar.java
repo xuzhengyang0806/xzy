bf21e17793bba39d1f5ff5939d29ae1b456b90ac
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-09 06:54:08 +0900

    韩顺平JAVA练习2

---
+++ ChangeChar.java
@@ -0,0 +1,18 @@
+public class ChangeChar {
+	public static void main(String[] args) {
+//对齐转义符
+		System.out.println("北京\t天津\t上海");
+//换行符		
+	    System.out.println("jack\nsmith\nmary");
+//想输出一个“\”	    
+	    System.out.println("C:\\Windows\\System32\\cmd.exe");
+//想输出两个“\\”
+	    System.out.println("C:\\\\Windows\\\\System32\\\\cmd.exe");
+//想要输出带有“”的句子
+	    System.out.println("老韩说\"要好好学习JAVA，有前途\"");
+//想要输出带有‘’的句子
+	    System.out.println("老韩说\'要好好学习JAVA，有前途\'");
+//后面替换前面
+	    System.out.println("JAVA教育\r坚持");
+	}
+}
