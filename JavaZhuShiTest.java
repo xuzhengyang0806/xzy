aad026262b0c8bc1c24b8d659b2e78f7e12fbc54
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-11 06:00:17 +0900

    韩顺平JAVA练习3

---
+++ JavaZhuShiTest.java
@@ -0,0 +1,15 @@
+/**
+ * 文档标签在eclipse中的练习
+ * ClassName:JavaZhuanYiTest 
+ * Description:
+ * @Date:2023年4月11日 上午5:55:33
+ * @author:徐正阳
+ * @version:1.0
+ */
+public class JavaZhuShiTest {
+	public static void main(String[] args) {
+		System.out.println("D:\\javacode>java Test");
+		System.out.println("书名\t作者\t价格\t销量");
+		System.out.println("三国\t罗贯中\t120\t1000");
+	}
+}
