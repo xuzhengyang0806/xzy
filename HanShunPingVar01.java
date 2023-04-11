6ea1e61851299addd5b84a3a7a94af407fb24f9d
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-12 06:40:11 +0900

    韩顺平JAVA练习4

---
+++ HanShunPingVar01.java
@@ -0,0 +1,13 @@
+public class HanShunPingVar01 {
+	public static void main(String[] args) {
+		int age = 30;
+		double score = 88.8;
+		char gender = '男';
+		String name = "king";
+		System.out.println("人的信息如下");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(score);
+		System.out.println(gender);
+	}
+}
