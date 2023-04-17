7c4008bb6f87e1208f4280ab4283caf2c7e20610
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-18 06:30:47 +0900

    韩顺平JAVA练习9

---
+++ HanShunPingAutoConvert.java
@@ -0,0 +1,9 @@
+public class HanShunPingAutoConvert {
+	public static void main(String[] args) {
+//		练习自动类型转换
+		int num = 'a';
+		double d1 = 80;
+		System.out.println(num);
+		System.out.println(d1);
+	}
+}
