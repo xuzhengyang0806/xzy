4f03bd9c55cb54b6726fb6bec652c3901c420e0e
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-18 06:55:09 +0900

    codecademy练习6

---
+++ BakeSale.java
@@ -0,0 +1,9 @@
+public class BakeSale {
+	public static void main(String[] args) {
+		int numCookies = 17;
+//		运用复合程序运算符来进行算术运算
+		numCookies -= 3;
+		numCookies /=2;
+		System.out.println(numCookies);
+	}
+}
