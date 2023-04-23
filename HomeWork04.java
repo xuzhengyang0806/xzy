4fd6cba7c45ad489900a2c0485408f899775da88
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-24 07:34:08 +0900

    韩顺平JAVA练习15

---
+++ HomeWork04.java
@@ -0,0 +1,10 @@
+public class HomeWork04 {
+	public static void main(String[] args) {
+		String name = "jack";
+		int age = 20;
+		double score = 99.8;
+		char gender = '男';
+		String like = "爱好";
+		System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n" + name + "\t" + age + "\t" + score +"\t"+ gender +"\t" +like);
+	}
+}
