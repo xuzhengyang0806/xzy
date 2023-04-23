938fb5d15428efa86bfc1b461d921019ac7aea29
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-04-24 06:58:19 +0900

    韩顺平JAVA练习14

---
+++ HomeWork03.java
@@ -0,0 +1,16 @@
+public class HomeWork03 {
+	public static void main(String[] args) {
+//		书名是多字符，多字符用字符串String;
+		String book1 = "天龙八部";
+		String book2 = "笑傲江湖";
+		System.out.println(book1 + book2);
+//		性别是单个字符，用Char类型；
+		char gender1 = '男';
+		char gender2 = '女';
+		System.out.println(gender1 + gender2);
+//		书的价格用整数类型或小数类型；
+		double price1 = 50.66;
+		double price2 = 23.89;
+		System.out.println(price1 + price2);
+	}
+}
