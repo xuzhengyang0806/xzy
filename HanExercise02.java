ab1f65e5a181750764935db013656411b2313b11
MICROSO-4JB9FJ9\Administrator <xzy19890806@gmail.com>
2023-06-02 08:00:30 +0900

    韩顺平JAVA练习49

---
+++ HanExercise02.java
@@ -0,0 +1,11 @@
+public class HanExercise02 {
+	public static void main(String[] args) {
+		for(char n1 = 'a';n1<='z';n1++) {
+			System.out.print(n1+" ");
+		}
+		System.out.println();
+		for(char n2 = 'Z';n2>='A';n2--) {
+			System.out.print(n2+" ");
+		}
+	}
+}
